ce643849b9f5360fe44524b8fab62a00c446625f
Raynald Mirville <raynaldm@pm.me>
2022-11-06 13:29:43 -0500

    change esrlintrc to cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier',
+	],
+	overrides: [
+		{
+			files: ['**/*.ts'],
+		},
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+	},
+	plugins: ['@typescript-eslint'],
+	root: true,
+};
