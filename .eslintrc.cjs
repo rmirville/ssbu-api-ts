3e3cca71f0502babe0a7d593dc89fa46853a5c3a
Raynald Mirville <raynaldm@pm.me>
2022-11-06 18:50:54 -0500

    exclude js files from tsconfig, eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,24 @@
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-		'prettier',
-	],
+	root: true,
+	env: {
+		node: true,
+		es2022: true,
+	},
 	overrides: [
 		{
 			files: ['**/*.ts'],
+			extends: [
+				'eslint:recommended',
+				'plugin:@typescript-eslint/recommended',
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+				'prettier',
+			],
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				tsconfigRootDir: __dirname,
+				project: ['./tsconfig.json'],
+			},
+			plugins: ['@typescript-eslint'],
 		},
 	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: ['./tsconfig.json'],
-	},
-	plugins: ['@typescript-eslint'],
-	root: true,
 };
