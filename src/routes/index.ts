6f8fe9cabd5adaa00ae8cb016b468637b6ee954d
Raynald Mirville <raynaldm@pm.me>
2022-11-07 22:37:35 -0500

    render homepage in json

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (_req, res) {
-	res.render('index', { title: 'Express' });
+	res.json({ title: 'Express' });
 });
 
 export default router;
