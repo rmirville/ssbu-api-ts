95627c3e820b25466c385920cab4a04f8e685593
Raynald Mirville <raynaldm@pm.me>
2022-11-08 09:10:02 -0500

    remove cookie-parser

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Express } from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-// import cookieParser from 'cookie-parser';
 // import logger from 'morgan';
 
 import indexRouter from './routes/index.js';
@@ -14,7 +13,6 @@ const app: Express = express();
 // app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
