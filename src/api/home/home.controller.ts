6c837bc668b58c6dd0978bfe3e90e7637220ef1c
Raynald Mirville <raynaldm@pm.me>
2022-11-13 15:04:49 -0500

    create bare index response

--- src/api/home/home.controller.ts
+++ src/api/home/home.controller.ts
@@ -2,6 +2,12 @@ import type { Request, Response } from 'express';
 
 export class HomeController {
 	public index(_req: Request, res: Response) {
-		res.json({ title: 'Express' });
+		res.json({
+			_links: {
+				self: {
+					href: 'https://api.ssbutools.com/v1/stages',
+				},
+			},
+		});
 	}
 }
