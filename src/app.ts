1185cfff166bfefd37115275d98a8caaf3e67cfc
Raynald Mirville <raynaldm@pm.me>
2022-11-12 20:04:19 -0500

    move server setup to App

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Express } from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import debugPkg from 'debug';
 import http from 'http';
 import morgan from 'morgan';
 import { port } from './config/config.app.js';
@@ -12,10 +13,12 @@ import usersRouter from './routes/users.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const logger = morgan;
+const { debug } = debugPkg;
+debug('api-ts:server');
 
 export class App {
 	public app: Express;
-	public server!: http.Server<
+	public server: http.Server<
 		typeof http.IncomingMessage,
 		typeof http.ServerResponse
 	>;
@@ -30,13 +33,60 @@ export class App {
 
 		this.app.use('/', indexRouter);
 		this.app.use('/users', usersRouter);
-		const server = http.createServer(this.app);
+		this.server = http.createServer(this.app);
 
 		/**
 		 * Listen on provided port, on all network interfaces.
 		 */
-		server.listen(port);
+		this.server.listen(port);
+		this.server.on('error', this.onError);
+		this.server.on('listening', this.onListening);
 	}
-}
 
-export const app: Express = new App().app;
+	/**
+	 * Event listener for HTTP server "error" event.
+	 */
+
+	onError = (error: { syscall: string; code: string }) => {
+		let exit = false;
+
+		if (error.syscall !== 'listen') {
+			throw error;
+		}
+
+		const bind =
+			typeof port === 'string'
+				? 'Pipe ' + port
+				: 'Port ' + JSON.stringify(port);
+
+		// handle specific listen errors with friendly messages
+		switch (error.code) {
+			case 'EACCES':
+				console.error(bind + ' requires elevated privileges');
+				exit = true;
+				break;
+			case 'EADDRINUSE':
+				console.error(bind + ' is already in use');
+				exit = true;
+				break;
+			default:
+				throw error;
+		}
+		if (exit) {
+			process.exit(1);
+		}
+	};
+
+	/**
+	 * Event listener for HTTP server "listening" event.
+	 */
+
+	onListening = () => {
+		const addr = this.server.address();
+		const bind =
+			typeof addr === 'string'
+				? 'pipe ' + addr
+				: 'port ' + JSON.stringify(addr?.port);
+		debug('Listening on ' + bind);
+	};
+}
--- src/index.ts
+++ src/index.ts
@@ -4,66 +4,6 @@
  * Module dependencies.
  */
 
-import * as appPkg from './app.js';
-import debugPkg from 'debug';
-import http from 'http';
-import type { Express } from 'express';
-import { port } from './config/config.app.js';
+import { App } from './app.js';
 
-const { debug } = debugPkg;
-debug('api-ts:server');
-
-const app: Express = appPkg.app;
-
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error: { syscall: string; code: string }) {
-	let exit = false;
-
-	if (error.syscall !== 'listen') {
-		throw error;
-	}
-
-	const bind =
-		typeof port === 'string' ? 'Pipe ' + port : 'Port ' + JSON.stringify(port);
-
-	// handle specific listen errors with friendly messages
-	switch (error.code) {
-		case 'EACCES':
-			console.error(bind + ' requires elevated privileges');
-			exit = true;
-			break;
-		case 'EADDRINUSE':
-			console.error(bind + ' is already in use');
-			exit = true;
-			break;
-		default:
-			throw error;
-	}
-	if (exit) {
-		process.exit(1);
-	}
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-	const addr = server.address();
-	const bind =
-		typeof addr === 'string'
-			? 'pipe ' + addr
-			: 'port ' + JSON.stringify(addr?.port);
-	debug('Listening on ' + bind);
-}
+new App();
