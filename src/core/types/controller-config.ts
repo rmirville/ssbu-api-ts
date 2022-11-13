0aae83c53a1feee4b3e5b147beb7eff2cb23628e
Raynald Mirville <raynaldm@pm.me>
2022-11-13 14:03:24 -0500

    add controller config types

---
+++ src/core/types/controller-config.ts
@@ -0,0 +1,6 @@
+import type { Controller } from './controller';
+
+export type ControllerConfig = {
+	path: string;
+	controller: Controller;
+};
---
+++ src/core/types/controller.ts
@@ -0,0 +1,5 @@
+import type { Router } from 'express';
+
+export interface Controller {
+	router: Router;
+}
